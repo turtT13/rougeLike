e8b056f16f2c7608f7a9018c11146bfab123828e
turT13 <nmoltrum709@gmail.com>
2022-05-31 09:59:08 -0700

    master

--- Player.py
+++ Player.py
@@ -3,8 +3,10 @@ import pygame
 class Player:
     def __init__(self, world):
         self.img = pygame.image.load(os.path.join('imgs', 'player.png'))
-        self.rect = self.img.get_rect()
+        self.rect = (0, 0, 32, 32)
         self.vel = 1
+
+
 #----------gets starting position---------#
         row_count = 0
         for row in world:
@@ -15,10 +17,17 @@ class Player:
                         row_count*64, col_count*64, 32, 32)
                 col_count += 1
             row_count += 1
+
+
+
     def get_rect(self):
         return self.rect
+
+
     def get_img(self):
         return self.img
+
+
     def movement(self, key):
         if key[pygame.K_w]:
             self.rect.y -= self.vel
