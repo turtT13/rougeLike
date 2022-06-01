158a25adcab0a48cc646d75253bc42d3cb15ab0c
turtT13 <nmoltrum709@gmail.com>
2022-05-31 22:31:36 -0700

    master

--- Player.py
+++ Player.py
@@ -28,7 +28,6 @@ class Player:
         return self.img
 
 
-
     def movement(self, key, world):
         if key[pygame.K_w]:
             self.rect.y -= self.vel
@@ -38,3 +37,15 @@ class Player:
             self.rect.y += self.vel
         if key[pygame.K_d]:
             self.rect.x += self.vel
+
+    def get_hits(self, world):
+        hits = []
+        for tile in world:
+            if self.rect.colliderect(tile[1]):
+                hits.append(tile[1])
+        return hits
+
+    def checkCollisionx(self, world):
+        collisions = self.get_hits(world)
+        for tile in collisions:
+            pass
